6bf1dc554644b9c669b65055c5aa7371b0e058fb
felix <knorr.felix@gmx.de>
2020-11-25 17:53:32 +0100

    fixes and extensions ...

--- pypf/abilities.py
+++ pypf/abilities.py
@@ -6,50 +6,51 @@ def ability_mod(val):
     return int((val - offset) / 2)
 
 
-def ability_detail(base, bonus):
-    final = base + bonus
+def ability_detail(base):
     return {
-        "Base": base, 
-        "Bonus": bonus,
-        "Final": final, 
-        "Mod": ability_mod(final)}
+        "Base": base,
+        "Mod": ability_mod(base)}
 
 
-def ability_table(str_base, str_adj, dex_base, dex_adj, con_base, con_adj, 
-                  int_base, int_adj, wis_base, wis_adj, cha_base, cha_adj):
+def ability_table(str_base, dex_base, con_base, int_base, wis_base,
+                  cha_base):
     ad = ability_detail
     return AttrDict({
-        'Str': ad(str_base, str_adj),
-        'Dex': ad(dex_base, dex_adj),
-        'Con': ad(con_base, con_adj),
-        'Int': ad(int_base, int_adj),
-        'Wis': ad(wis_base, wis_adj),
-        'Cha': ad(cha_base, cha_adj)
+        'Str': ad(str_base),
+        'Dex': ad(dex_base),
+        'Con': ad(con_base),
+        'Int': ad(int_base),
+        'Wis': ad(wis_base),
+        'Cha': ad(cha_base)
     })
 
 
 def spells_per_day_bonus(mod, lvl):
     if mod <= 0: return 0
     elif mod == 1:
-        if lvl == 1: return 1
+        return 1 if lvl == 1 else 0
     elif mod == 2:
-        if lvl in (1, 2): return 1
+        return 1 if lvl in (1, 2) else 0
     elif mod == 3:
-        if lvl in (1, 2, 3): return 1
+        return 1 if lvl in (1, 2, 3) else 0
     elif mod == 4:
-        if lvl in (1, 2, 3, 4): return 1 
+        return 1  if lvl in (1, 2, 3, 4) else 0
     elif mod == 5:
         if lvl == 1: return 2
         elif lvl in (2, 3, 4, 5): return 1
+        else: return 0
     elif mod == 6:
         if lvl in (1, 2): return 2
         elif lvl in (3, 4, 5, 6): return 1
+        else: return 0
     elif mod == 7:
         if lvl in (1, 2, 3): return 2
         elif lvl in (4, 5, 6, 7): return 1
+        else: return 0
     elif mod == 8:
         if lvl in (1, 2, 3, 4): return 2
         elif lvl in (5, 6, 7, 8): return 1
+        else: return 0
     elif mod == 9:
         if level in (1,): return 3
         elif level in (2, 3, 4, 5): return 2
--- pypf/skills.py
+++ pypf/skills.py
@@ -22,7 +22,7 @@ all_skills = [make_skill(name, mod, untrained) for name, mod, untrained in [
     ("Disguise", "Cha", True),
     ("Escape artist", "Dex", True),
     ("Fly", "Dex", True),
-    ("Handle", "Cha", False),
+    ("Handle animal", "Cha", False),
     ("Heal", "Wis", True),
     ("Intimidate", "Cha", True),
     ("Knowledge (arcana)", "Int", False),
@@ -49,20 +49,39 @@ all_skills = [make_skill(name, mod, untrained) for name, mod, untrained in [
     ("Use magic device", "Cha", False)
 ]]
 
+skill_names = [s.name for s in all_skills]
+
+
 def make_class_skill_mask(*names):
-    return [True if any(re.match(pattern, skill.name) for pattern in names) 
+    invalid_names = [n for n in names if not any(re.match(n, skname)
+                                                 for skname in skill_names)]
+    if invalid_names:
+        raise ValueError(f"Not a skill: {invalid_names}")
+
+    return [True if any(re.match(pattern, skill.name) for pattern in names)
             else False
             for skill in all_skills]
-            
+
+
 class_skill_masks = {
     "Sorcerer": make_class_skill_mask(
-        'Appraise', 'Bluff', 'Craft', 'Fly', 'Intimidate', 'Knowledge (arcana)',
+        'Appraise', 'Bluff', 'Craft', 'Fly', 'Intimidate',
+        r'Knowledge \(arcana\)',
         'Profession', 'Spellcraft', 'Use magic device'),
     "Draconic": make_class_skill_mask('Perception'),
     "Dragon Disciple": make_class_skill_mask(
-        'Diplomacy', 'Escape artist', 'Fly', 'Knowledge.*', 'Perception', 
-        'Spellcraft')
+        'Diplomacy', 'Escape artist', 'Fly', 'Knowledge.*', 'Perception',
+        'Spellcraft'),
+    "Twilight Assassin": make_class_skill_mask(
+        'Acrobatics', 'Bluff', 'Climb', 'Disguise', 'Escape artist',
+        'Intimidate', 'Perception', 'Sense motive', 'Sleight of hand',
+        'Stealth', 'Swim', r'Knowledge \(local\)'),
+    "Druid": make_class_skill_mask(
+        "Climb", "Craft", "Fly", "Handle animal", "Heal", 
+        r"Knowledge \(geography\)", r"Knowledge \(nature\)", "Perception",
+        "Profession", "Ride", "Spellcraft", "Survival", "Swim")
 }
+
 
 def combined_class_skill_mask(classes):
     masks = [class_skill_masks[class_] for class_ in classes]
@@ -70,11 +89,20 @@ def combined_class_skill_mask(classes):
 
 
 def make_skill_table(input_dict, abilities, *classes):
+    wrong_keys = [key for key in input_dict if key not in skill_names]
+    if wrong_keys:
+        print("The following keys are probably misstyped:",
+              wrong_keys)
+        exit(1)
+    if not all(cl in class_skill_masks for cl in classes):
+        print("You either misstyped your class, or it is not yet supported")
+        exit(1)
+
     in_dict = defaultdict(int, input_dict)
-    contained_mask = (True if in_dict[skill.name] > 0 or skill.untrained 
-                        else False for skill in all_skills)
+    contained_mask = (True if in_dict[skill.name] > 0 or skill.untrained
+                      else False for skill in all_skills)
     mask = combined_class_skill_mask(classes)
-    return {skill.name: in_dict[skill.name] 
+    return {skill.name: in_dict[skill.name]
                         + abilities[skill.ability]['Mod']
                         + (3 if is_cs and in_dict[skill.name] else 0)
             for skill, is_cs, contained in zip(all_skills, mask, contained_mask)
